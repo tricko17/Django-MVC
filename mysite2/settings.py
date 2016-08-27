ffabcf07afe1b270fd3f913dad6efd5ac93d558f
tricko17 <rioookta@gmail.com>
2016-08-27 23:31:23 +0700

    remove static file dir

--- mysite2/settings.py
+++ mysite2/settings.py
@@ -113,11 +113,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/mystatic")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
