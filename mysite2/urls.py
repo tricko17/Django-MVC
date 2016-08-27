abe656d45692126d97eda0d1a75217a0e7c98ed4
tricko17 <rioookta@gmail.com>
2016-08-27 23:54:42 +0700

    add index

--- mysite2/urls.py
+++ mysite2/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 
 urlpatterns = [
+	url(r'^blog/', include('blog.urls',namespace='blogfree')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls',namespace='blog')),
 ]
